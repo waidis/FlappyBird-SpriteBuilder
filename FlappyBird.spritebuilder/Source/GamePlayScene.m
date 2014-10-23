010a6baad97469c21ccd61e2ff26c66e6b9322b9
Vaidotas Urbanavicius <vaidotas.urbanavicius@gmail.com>
2014-10-23 11:02:35 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,9 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"]; // Will load a new Character from the CCB file we created for you named Character. CCB files are created by SpriteBuilder and describe the properties and appearance of in-game objects like our Flappy Bird.
+    
+    [physicsNode addChild:character]; // will add your character as a child of physicsNode so that physics will be applied to it.
 }
 
 -(void)update:(CCTime)delta
@@ -14,6 +17,14 @@
     // put update code here
 }
 
-// put new methods here
+/*
+ This method is part of the Cocos2D game engine and whenever the player touches the screen, this method is called automatically -- all you need to do is fill it in.
+ */
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    
+    [character flap];
+    //Now, whenever a new touch begins, we will call the 'flap' method on our character which will apply an impulse to our character. This will be simulated by the physicsNode and the character will fly upwards.
+}
 
 @end
